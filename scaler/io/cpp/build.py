97151773b0a2cdfcde02ae86c0105cf88d2b2bf0
magniloquency <197707854+magniloquency@users.noreply.github.com>
2025-03-14 21:45:23 -0400

    clang support

--- scaler/io/cpp/build.py
+++ scaler/io/cpp/build.py
@@ -7,12 +7,18 @@ with open("src/defs.h") as f:
 
 builder.cdef(defs)
 
+import os
+
 builder.set_source(
     "cpp",
     '#include "../src/main.hpp"',
     source_extension=".cpp",
     # runs gcc like it's g++ and link the C++ standard library
-    extra_compile_args=["-xc++", "-lstdc++", "-shared-libgcc", "-std=gnu++23", "-Wall", "-Wextra", "-Werror"],
+    extra_compile_args=(
+        ["-std=c++23", "-Wall", "-Wextra", "-pedantic"]
+        if os.getenv("CC") == "clang"
+        else ["-xc++", "-lstdc++", "-shared-libgcc", "-std=gnu++23", "-Wall", "-Wextra", "-Werror"]
+    ),
     language="c++",
 )
 
