cf9d1a6d53bbd09b9416e8f39017e4bf633b0a2d
gxu <georgexu420@gmail.com>
2025-03-28 21:27:30 +0800

    Remove redundant imports and format code
    Signed-off-by: gxu <georgexu420@gmail.com>

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,35 +1,8 @@
-import functools
 import os
-import random
-import runpy
-import time
 import unittest
-from concurrent.futures import CancelledError
 
-from scaler import Client, SchedulerClusterCombo
-from scaler.utility.exceptions import MissingObjects, ProcessorDiedError
-from scaler.utility.logging.scoped_logger import ScopedLogger
 from scaler.utility.logging.utility import setup_logger
-from scaler.utility.network_util import get_available_tcp_port
 from tests.utility import logging_test_name
-
-
-def noop(sec: int):
-    return sec * 1
-
-
-def noop_sleep(sec: int):
-    time.sleep(sec)
-    return sec
-
-
-def heavy_function(sec: int, payload: bytes):
-    return len(payload) * sec
-
-
-def raise_exception(foo: int):
-    if foo == 11:
-        raise ValueError("foo cannot be 100")
 
 
 class TestExamples(unittest.TestCase):
