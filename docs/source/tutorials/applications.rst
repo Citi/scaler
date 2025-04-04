9d3f7fcb5bdeb71bf3a56423644daf9334a829c8
gxu <georgexu420@gmail.com>
2025-04-05 06:30:47 +0800

    Add another real life example (getting option price)
    Get option closing price with yfinance. Also:
     - Update docs
     - Update workflow
     - Update requirements
    
    Signed-off-by: gxu <georgexu420@gmail.com>

--- docs/source/tutorials/applications.rst
+++ docs/source/tutorials/applications.rst
@@ -13,9 +13,20 @@ Calculate Implied Volatility
 This example calculate implied volatility for many given market price. We use `client.submit` to achieve such goal.
 Alternatively, one may use `client.map` if their computation function is heavier than what's shown in the example.
 Remember, `client.map` makes sense only in two cases: A. The body of the function you are submitting is quite large; 
-B. The function is very computation heavy.
+B. The function is very computation heavy or is possible to block.
 
 .. literalinclude:: ../../../examples/applications/implied_volatility.py
    :language: python
+
+
+Get Option Closing Price
+------------------------
+
+This example gets option closing price for a specified ticket and the start date. We use `client.map` to reduce the
+overhead introduced by slow IO speed.
+
+.. literalinclude:: ../../../examples/applications/yfinance_historical_price.py
+   :language: python
+
 
 
---
+++ examples/applications/yfinance_historical_price.py
@@ -0,0 +1,82 @@
+
+import yfinance as yf
+import datetime
+import math
+from scaler import Client
+from scaler.cluster.combo import SchedulerClusterCombo
+
+def get_option_data(stock_symbol, expiration_date, option_type, strike):
+    stock = yf.Ticker(stock_symbol)
+    option_chain = stock.option_chain(expiration_date)
+    options = getattr(option_chain, "calls" if option_type.startswith("call") else "puts")
+    option_data = options[options["strike"] == strike]
+    return option_data
+
+
+def get_option_history_data(contract_symbol, days_before_expiration=30):
+    option = yf.Ticker(contract_symbol)
+    option_info = option.info
+    option_expiration_date = datetime.datetime.fromtimestamp(option_info["expireDate"])
+
+    start_date = option_expiration_date - datetime.timedelta(days=days_before_expiration)
+    option_history = option.history(start=start_date)
+    return option_history
+
+
+
+def main():
+    cluster = SchedulerClusterCombo(n_workers=2)
+    client = Client(address=cluster.get_address())
+
+    # Example:
+    stock_symbol = "AAPL"
+    expiration_date = "2023-10-27"
+    expiration_date = None
+    option_type = "call"
+    # strike = 170.0
+
+    def func(strike):
+        option_data = get_option_data(stock_symbol, expiration_date, option_type, strike)
+        for _, od in option_data.iterrows():
+            contract_symbol = od["contractSymbol"]
+            option_history = get_option_history_data(contract_symbol)
+            first_option_history = option_history.iloc[0]
+            first_option_history_date = option_history.index[0]
+            first_option_history_close = first_option_history["Close"]
+            return (contract_symbol, first_option_history_close, first_option_history_date)
+    # Original version, without scaler. Wall time: 44.487 s
+    # for strike in range(170, 250): 
+    #     res = func(strike)
+    #     if res is None:
+    #         continue
+    #     contract_symbol = res[0]
+    #     first_option_history_close = res[1]
+    #     first_option_history_date = res[2]
+    #     print("For {}, the closing price was ${:.2f} on {}.".format(
+    #                                                                 contract_symbol,
+    #                                                                 first_option_history_close,
+    #                                                                 first_option_history_date
+    #          ))
+
+    # With scaler, 32.382 s
+    results = client.map(func, [(strike,) for strike in range(170, 250)])
+    for res in results:
+        if res is None: 
+            continue
+        contract_symbol = res[0]
+        first_option_history_close = res[1]
+        first_option_history_date = res[2]
+        
+        print("For {}, the closing price was ${:.2f} on {}.".format(
+                                                                    contract_symbol,
+                                                                    first_option_history_close,
+                                                                    first_option_history_date
+             ))
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
