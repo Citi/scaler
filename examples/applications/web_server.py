b7a79975ddf21b58cf69fc3921c89be1f4a93ff4
magniloquency <197707854+magniloquency@users.noreply.github.com>
2025-04-16 10:33:40 -0400

    Add web server example to ci
    Signed-off-by: magniloquency <197707854+magniloquency@users.noreply.github.com>

--- examples/applications/web_server.py
+++ examples/applications/web_server.py
@@ -1,8 +1,10 @@
 import nest_asyncio
 import sys
+import time
 import grequests
 import requests
 import asyncio
+import multiprocessing
 from psutil import cpu_count
 from timeit import default_timer as timer
 from quart import Quart
@@ -24,30 +26,63 @@ def server(scaled: bool):
             response: requests.Response = await asyncio.wrap_future(future)
             return response.text
 
-        app.run(loop=asyncio.get_event_loop())
+        app.run(loop=asyncio.get_event_loop(), use_reloader=False)
         cluster.shutdown()
     else:
         @app.route("/")
         def root():
             return requests.get("https://example.com").text
 
-        app.run()
+        app.run(use_reloader=False)
 
 
-def client():
+def client() -> float:
     start = timer()
-    requests = [grequests.get("http://127.0.0.1:5000") for _ in range(1000)]
+    requests = [grequests.get("http://127.0.0.1:5000") for _ in range(500)]
     _ = grequests.map(requests)
-    print(f"Time: {timer() - start:.2f}s")
+    return timer() - start
+
+
+def client_multiprocess(result: multiprocessing.Queue):
+    t = client()
+    result.put(t)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2 and sys.argv[1] == "server" and sys.argv[2] == "scaled":
+    if len(sys.argv) == 1:
+        print("no args provided, speed testing")
+
+        server_ = multiprocessing.Process(target=server, args=(False,))
+        server_.start()
+
+        result = multiprocessing.Queue()
+        client_ = multiprocessing.Process(target=client_multiprocess, args=(result,))
+        client_.start()
+        client_.join()
+        server_.kill()
+        server_.join()
+        nonscaled = result.get()
+
+        server_ = multiprocessing.Process(target=server, args=(True,))
+        server_.start()
+        time.sleep(5)
+        client_ = multiprocessing.Process(target=client_multiprocess, args=(result,))
+        client_.start()
+        client_.join()
+        server_.kill()
+        server_.join()
+        scaled = result.get()
+        print(f"Non-scaled time: {nonscaled:.2f}s")
+        print(f"Scaled time: {scaled:.2f}s")
+        print(f"Speedup: {nonscaled / scaled:.2f}x")
+    elif len(sys.argv) == 2 and sys.argv[1] == "server" and sys.argv[2] == "scaled":
         server(scaled=True)
     elif len(sys.argv) == 2 and sys.argv[1] == "server" and sys.argv[2] == "unscaled":
         server(scaled=False)
     elif len(sys.argv) == 2 and sys.argv[1] == "client":
-        client()
+        elapsed = client()
+
+        print(f"Time: {elapsed:.2f}s")
     else:
         print("Usage: web_server.py server|client [scaled|unscaled]")
         sys.exit(1)
