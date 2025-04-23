edae82ba55611e02ebbe56edaecd3fad69222f53
gxu <georgexu420@gmail.com>
2025-04-24 01:40:17 +0800

    Try this new method of downloading dependencies
    Signed-off-by: gxu <georgexu420@gmail.com>

---
+++ download_dependencies.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "$1" == "boost" ]; then
+	curl -L https://archives.boost.io/release/1.88.0/source/boost_1_88_0.tar.gz -o boost188.tar.gz
+	tar -xzf boost188.tar.gz
+	sudo mkdir -p /usr/include/boost
+	sudo mv boost_1_88_0/boost /usr/include/.
+	echo "Installed Boost into third_party/include/boost"
+
+elif [ "$1" == "capnp" ]; then
+	curl -O https://capnproto.org/capnproto-c++-1.1.0.tar.gz
+	tar zxf capnproto-c++-1.1.0.tar.gz
+	cd capnproto-c++-1.1.0
+	./configure --prefix=/usr/
+	make -j6 check
+	sudo make install
+	echo "Installed capnp into third_party/include/capnp"
+
+else 
+    echo "Usage: ./download_dependencies.sh [boost|capnp]"
+    exit 1
+fi
+
