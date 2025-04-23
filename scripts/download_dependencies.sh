63d75a813aae652f8e537c175fa18f0d6f742de6
gxu <georgexu420@gmail.com>
2025-04-24 06:35:35 +0800

    Parametrize dependencies' version
    Signed-off-by: gxu <georgexu420@gmail.com>

---
+++ scripts/download_dependencies.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Remember:
+#	Update the usage string when you are add/remove dependency
+#	Bump version should be done through variables, not hard coded strs.
+
+BOOST_VERSION="1.88.0"
+CAPNP_VERSION="1.1.0"
+
+if [ "$1" == "boost" ]; then
+	BOOST_FOLDER_NAME="boost_$(echo $BOOST_VERSION | tr '.' '_')"
+	BOOST_PACKAGE_NAME=${BOOST_FOLDER_NAME}.tar.gz
+	curl -O https://archives.boost.io/release/${BOOST_VERSION}/source/${BOOST_PACKAGE_NAME}
+	tar -xzf ${BOOST_PACKAGE_NAME}
+	sudo mkdir -p /usr/include/boost
+	sudo mv ${BOOST_FOLDER_NAME}/boost /usr/include/.
+	echo "Installed Boost into /usr/include/boost"
+
+elif [ "$1" == "capnp" ]; then
+	CAPNP_FOLDER_NAME="capnproto-c++-$(echo $CAPNP_VERSION)"
+	CAPNP_PACKAGE_NAME=${CAPNP_FOLDER_NAME}.tar.gz
+	curl -O https://capnproto.org/${CAPNP_PACKAGE_NAME}
+	tar zxf ${CAPNP_PACKAGE_NAME}
+	cd ${CAPNP_FOLDER_NAME}
+	./configure --prefix=/usr/
+	make -j6 check
+	sudo make install
+	echo "Installed capnp into /usr"
+
+else 
+    echo "Usage: ./download_dependencies.sh [boost|capnp]"
+    exit 1
+fi
+
