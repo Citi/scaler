ba5188a6f35b70fdf332a782682fadedbb4d2bee
gxu <georgexu420@gmail.com>
2025-04-25 06:03:51 +0800

    Add entry point for object storage server
    Signed-off-by: gxu <georgexu420@gmail.com>

--- scaler/entry_points/object_storage_server.py
+++ scaler/entry_points/object_storage_server.py
@@ -1,8 +1,12 @@
 import argparse
+
 from scaler.object_storage.object_storage_server import run_object_storage_server
 
+
 def get_args():
-    parser = argparse.ArgumentParser("scaler object storage server", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser = argparse.ArgumentParser(
+        "scaler object storage server", formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
     parser.add_argument(
         "--name",
         "-n",
@@ -11,17 +15,13 @@ def get_args():
         help="Specify name the server will be listening to. Can be IP or NS record. Default to 127.0.0.1",
     )
     parser.add_argument(
-        "--port",
-        "-p",
-        type=str,
-        default="55555",
-        help="Specify port the server will be listening to. Default to 55555",
+        "--port", "-p", type=str, default="55555", help="Specify port the server will be listening to. Default to 55555"
     )
     parser.add_argument(
         "--library-path",
         "-i",
         type=str,
-        # default="55555",
+        required=True,
         help="Specify the path of underlying server implementation the program will use",
     )
     return parser.parse_args()
@@ -30,4 +30,3 @@ def get_args():
 def main():
     args = get_args()
     run_object_storage_server(args.library_path, args.name, args.port)
-
--- scaler/object_storage/object_storage_server.py
+++ scaler/object_storage/object_storage_server.py
@@ -1,6 +1,7 @@
 import ctypes
 import sys
 
+
 def load_library(lib_path):
     """
     Load the shared dynamic library.
@@ -11,6 +12,7 @@ def load_library(lib_path):
         print(f"Failed to load library: {e}")
         sys.exit(1)
 
+
 def run_object_storage_server(lib_path: str, name: str = "127.0.0.1", port: str = "55555"):
     # Load the library
     lib = load_library(lib_path=lib_path)
@@ -19,8 +21,7 @@ def run_object_storage_server(lib_path: str, name: str = "127.0.0.1", port: str 
     lib.run_object_storage_server.restype = None
 
     # Call the function
-    name_bytes = name.encode('utf-8')
-    port_bytes = port.encode('utf-8')
+    name_bytes = name.encode("utf-8")
+    port_bytes = port.encode("utf-8")
 
     lib.run_object_storage_server(name_bytes, port_bytes)
-
