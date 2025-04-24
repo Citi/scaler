d8d7573e6840a85a4bda227a4c8bbc5e0356ff01
gxu <georgexu420@gmail.com>
2025-04-24 10:39:01 +0800

    Fix mypy linter
    Signed-off-by: gxu <georgexu420@gmail.com>

--- scripts/download_dependencies.sh
+++ scripts/download_dependencies.sh
@@ -14,17 +14,19 @@ if [ "$1" == "boost" ]; then
 	sudo mkdir -p /usr/include/boost
 	sudo mv ${BOOST_FOLDER_NAME}/boost /usr/include/.
 	echo "Installed Boost into /usr/include/boost"
+	sudo rm -rf ${BOOST_FOLDER_NAME} ${BOOST_PACKAGE_NAME}
 
 elif [ "$1" == "capnp" ]; then
 	CAPNP_FOLDER_NAME="capnproto-c++-$(echo $CAPNP_VERSION)"
 	CAPNP_PACKAGE_NAME=${CAPNP_FOLDER_NAME}.tar.gz
 	curl -O https://capnproto.org/${CAPNP_PACKAGE_NAME}
-	tar zxf ${CAPNP_PACKAGE_NAME}
+	tar -xzf ${CAPNP_PACKAGE_NAME}
 	cd ${CAPNP_FOLDER_NAME}
 	./configure --prefix=/usr/
 	make -j6 check
 	sudo make install
 	echo "Installed capnp into /usr"
+	sudo rm -rf ${CAPNP_FOLDER_NAME} ${CAPNP_PACKAGE_NAME}
 
 else 
     echo "Usage: ./download_dependencies.sh [boost|capnp]"
