233391a416342237b374fbf7a5a433306a409787
gxu <georgexu420@gmail.com>
2025-04-29 03:08:39 +0800

    Make github action cache dependencies
    Another try to make github action cache dependencies
    This time, we do not cache system library. Instead, we cache local
    directory and only cache the compile steps. Installing is not taking a
    lot of time anyway.
    
    Signed-off-by: gxu <georgexu420@gmail.com>

--- scripts/download_install_dependencies.sh
+++ scripts/download_install_dependencies.sh
@@ -7,29 +7,41 @@ BOOST_VERSION="1.88.0"
 CAPNP_VERSION="1.1.0"
 
 if [ "$1" == "boost" ]; then
-	BOOST_FOLDER_NAME="boost_$(echo $BOOST_VERSION | tr '.' '_')"
-	BOOST_PACKAGE_NAME=${BOOST_FOLDER_NAME}.tar.gz
-	curl -O https://archives.boost.io/release/${BOOST_VERSION}/source/${BOOST_PACKAGE_NAME} --retry 100 --retry-max-time 3600
-	tar -xzf ${BOOST_PACKAGE_NAME}
-	sudo mkdir -p /usr/include/boost
-	sudo mv ${BOOST_FOLDER_NAME}/boost /usr/include/.
-	echo "Installed Boost into /usr/include/boost"
-	sudo rm -rf ${BOOST_FOLDER_NAME} ${BOOST_PACKAGE_NAME}
+	if [ "$2" == "compile" ]; then
+		BOOST_FOLDER_NAME="boost_$(echo $BOOST_VERSION | tr '.' '_')"
+		BOOST_PACKAGE_NAME=${BOOST_FOLDER_NAME}.tar.gz
+		curl -O https://archives.boost.io/release/${BOOST_VERSION}/source/${BOOST_PACKAGE_NAME} --retry 100 --retry-max-time 3600
+		tar -xzf ${BOOST_PACKAGE_NAME}
+		mv ${BOOST_FOLDER_NAME} boost
+	elif [ "$2" == "install" ]; then
+		sudo cp -r boost/boost /usr/include/.
+		echo "Installed Boost into /usr/include/boost"
+	else 
+		echo "Argument needs to be either compile or install"
+		exit 1
+	fi
 
 elif [ "$1" == "capnp" ]; then
-	CAPNP_FOLDER_NAME="capnproto-c++-$(echo $CAPNP_VERSION)"
-	CAPNP_PACKAGE_NAME=${CAPNP_FOLDER_NAME}.tar.gz
-	curl -O https://capnproto.org/${CAPNP_PACKAGE_NAME} --retry 100 --retry-max-time 3600
-	tar -xzf ${CAPNP_PACKAGE_NAME}
-	cd ${CAPNP_FOLDER_NAME}
-	./configure --prefix=/usr/
-	make -j6 check
-	sudo make install
-	echo "Installed capnp into /usr"
-	sudo rm -rf ${CAPNP_FOLDER_NAME} ${CAPNP_PACKAGE_NAME}
+	if [ "$2" == "compile" ]; then
+		CAPNP_FOLDER_NAME="capnproto-c++-$(echo $CAPNP_VERSION)"
+		CAPNP_PACKAGE_NAME=${CAPNP_FOLDER_NAME}.tar.gz
+		curl -O https://capnproto.org/${CAPNP_PACKAGE_NAME} --retry 100 --retry-max-time 3600
+		tar -xzf ${CAPNP_PACKAGE_NAME}
+		mv ${CAPNP_FOLDER_NAME} capnp
+		cd capnp
+		./configure --prefix=/usr/
+		make -j6 check
+	elif [ "$2" == "install" ]; then
+		cd capnp
+		sudo make install
+		echo "Installed capnp into /usr"
+	else 
+		echo "Argument needs to be either compile or install"
+		exit 1
+	fi
 
 else 
-    echo "Usage: ./download_install_dependencies.sh [boost|capnp]"
+    echo "Usage: ./download_install_dependencies.sh [boost|capnp] [compile|install]"
     exit 1
 fi
 
