4e94b2fb9e3baecdf68d12501d8d5483be6d5c17
cloakedclock <213850831+cloakedclock@users.noreply.github.com>
2025-05-28 17:27:38 -0400

    Fix webui worker_processor errors (#124)
    * fix webui errors
    
    Signed-off-by: cloakedclock <213850831+cloakedclock@users.noreply.github.com>
    
    * remove magic numbers
    
    Signed-off-by: cloakedclock <213850831+cloakedclock@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: cloakedclock <213850831+cloakedclock@users.noreply.github.com>

--- scaler/ui/worker_processors.py
+++ scaler/ui/worker_processors.py
@@ -1,5 +1,6 @@
 import dataclasses
 from typing import Dict, List, Optional
+from threading import Lock
 
 from nicegui import ui
 from nicegui.element import Element
@@ -10,37 +11,30 @@ from scaler.ui.utility import format_worker_name
 
 @dataclasses.dataclass
 class WorkerProcessors:
-    workers: Dict[bytes, "WorkerProcessorTable"] = dataclasses.field(default_factory=dict)
+    workers: Dict[str, "WorkerProcessorTable"] = dataclasses.field(default_factory=dict)
+    _lock: Lock = Lock()
 
-    _parent: Optional[Element] = dataclasses.field(default=None)
-
-    def draw_section(self, parent: Element):
-        self._parent = parent
-        for processor_table in self.workers.values():
-            processor_table.draw_table()
+    @ui.refreshable
+    def draw_section(self):
+        with self._lock:
+            for processor_table in self.workers.values():
+                processor_table.draw_table()
 
     def update_data(self, data: List[WorkerStatus]):
-        assert self._parent is not None
-
-        previous_worker_ids = set(self.workers.keys())
-
-        with self._parent:
+        with self._lock:
             for worker in data:
                 worker_name = worker.worker_id.decode()
-
-                processor_table = self.workers.get(worker.worker_id)
+                processor_table = self.workers.get(worker_name)
 
                 if processor_table is None:
                     processor_table = WorkerProcessorTable(worker_name, worker.rss_free, worker.processor_statuses)
-                    processor_table.draw_table()
-                    self.workers[worker.worker_id] = processor_table
+                    self.workers[worker_name] = processor_table
                 elif processor_table.processor_statuses != worker.processor_statuses:
                     processor_table.processor_statuses = worker.processor_statuses
-                    processor_table.draw_table.refresh()
 
-            removed_workers = previous_worker_ids - set(worker.worker_id for worker in data)
-            for worker_id in removed_workers:
-                self.workers.pop(worker_id).delete_row()
+    def remove_worker(self, dead_worker: str):
+        with self._lock:
+            self.workers.pop(dead_worker, None)
 
 
 @dataclasses.dataclass
@@ -51,7 +45,6 @@ class WorkerProcessorTable:
 
     handler: Optional[Element] = dataclasses.field(default=None)
 
-    @ui.refreshable
     def draw_table(self):
         formatted_worker_name = format_worker_name(self.worker_name)
         with ui.card().classes("w-full") as handler:
@@ -75,7 +68,7 @@ class WorkerProcessorTable:
 
     @staticmethod
     def draw_row(processor_status: ProcessorStatus, rss_free: int):
-        cpu = processor_status.resource.cpu
+        cpu = processor_status.resource.cpu / 10
         rss = int(processor_status.resource.rss / 1e6)
         rss_free = int(rss_free / 1e6)
 
