819a331093533a4ee7954ff9795afda0d44d832b
gxu <georgexu420@gmail.com>
2025-06-02 05:45:48 +0800

    Resolve comment - Use version.txt to specify version for this project
    Signed-off-by: gxu <georgexu420@gmail.com>

--- scaler/about.py
+++ scaler/about.py
@@ -1,1 +1,5 @@
-__version__ = "1.9.9"
+try:
+    with open("version.txt", "r") as f:
+        __version__ = f.read().strip()
+except FileNotFoundError as e:
+    raise FileNotFoundError(f"Failed to find version.txt") from e
