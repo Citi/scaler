fa81947ad5c9cd0a8b7f3d3e976bcd3e4719b83f
gxu <georgexu420@gmail.com>
2025-06-02 09:07:18 +0800

    Resolve comment - remove redundant exception
    Signed-off-by: gxu <georgexu420@gmail.com>

--- scaler/about.py
+++ scaler/about.py
@@ -1,5 +1,2 @@
-try:
-    with open("version.txt", "r") as f:
-        __version__ = f.read().strip()
-except FileNotFoundError as e:
-    raise FileNotFoundError(f"Failed to find version.txt") from e
+with open("version.txt", "r") as f:
+    __version__ = f.read().strip()
