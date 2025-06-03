80e3c0be231a8110b2f5360dd83bab26196f39ee
gxu <georgexu420@gmail.com>
2025-06-03 23:35:45 +0800

    Always perform debug build
    Signed-off-by: gxu <georgexu420@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,6 @@
 #!/bin/bash -e
 
-if [[ "$1" != "Debug" && "$1" != "Release" ]]; then
-    echo "Usage: ./scripts/build.sh [Debug|Release]"
-    exit 1
-fi
-
-BUILD_TYPE="$1"
-cmake -S . -B build -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug
 cd build && make
 cp scaler/io/ymq/libymq.so ../scaler/io/ymq/.
 cp scaler/object_storage/*.so ../scaler/object_storage/.
