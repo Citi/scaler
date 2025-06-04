8d474235baa1512a5538fa9685099391bd965af7
gxu <georgexu420@gmail.com>
2025-06-05 02:02:24 +0800

    use relative directory in aboutpu
    Signed-off-by: gxu <georgexu420@gmail.com>

--- scaler/about.py
+++ scaler/about.py
@@ -1,2 +1,5 @@
-with open("version.txt", "r") as f:
+import os
+
+version_file_path = os.path.join(os.path.dirname(__file__), "version.txt")
+with open(version_file_path, "r") as f:
     __version__ = f.read().strip()
