dc4babe6a9b8df235e917f98d661c3b5bad32cc5
magniloquency <197707854+magniloquency@users.noreply.github.com>
2025-06-05 14:19:41 -0400

    restore build script
    Signed-off-by: magniloquency <197707854+magniloquency@users.noreply.github.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-cmake -S . -B build -DCMAKE_CXX_COMPILER=$(which clang++) -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=.
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=.
 cd build && make
 make install
 ctest
