1e74e34baceae653d7f779cd6e007be78be6fb5b
magniloquency <197707854+magniloquency@users.noreply.github.com>
2025-06-09 22:36:58 -0400

    stub send method
    Signed-off-by: magniloquency <197707854+magniloquency@users.noreply.github.com>

--- scaler/io/ymq/pymod_ymq/io_socket.h
+++ scaler/io/ymq/pymod_ymq/io_socket.h
@@ -27,7 +27,10 @@ static void PyIOSocket_dealloc(PyIOSocket* self) {
     Py_TYPE(self)->tp_free((PyObject*)self);  // Free the PyObject
 }
 
-// static PyObject* PyIOSocket_send(PyIOSocket* self, PyObject* args) {}
+static PyObject* PyIOSocket_send(PyIOSocket* self, PyObject* args, PyObject* kwargs) {
+    PyErr_SetString(PyExc_NotImplementedError, "send() is not implemented yet");
+    return nullptr;
+}
 
 static PyObject* PyIOSocket_repr(PyIOSocket* self) {
     return PyUnicode_FromFormat("<IOSocket at %p>", (void*)self->socket.get());
@@ -42,13 +45,16 @@ static PyGetSetDef PyIOSocket_properties[] = {
     {"identity", (getter)PyIOSocket_identity_getter, nullptr, PyDoc_STR("Get the identity of the IOSocket"), nullptr},
     {nullptr, nullptr, nullptr, nullptr, nullptr}};
 
-static PyMethodDef PyIOSocket_methods[] = {{nullptr, nullptr, 0, nullptr}};
+static PyMethodDef PyIOSocket_methods[] = {
+    {"send", (PyCFunction)PyIOSocket_send, METH_VARARGS | METH_KEYWORDS, PyDoc_STR("Send data through the IOSocket")},
+    {nullptr, nullptr, 0, nullptr}};
 
 static PyType_Slot PyIOSocket_slots[] = {
     {Py_tp_init, (void*)PyIOSocket_init},
     {Py_tp_dealloc, (void*)PyIOSocket_dealloc},
     {Py_tp_repr, (void*)PyIOSocket_repr},
     {Py_tp_getset, (void*)PyIOSocket_properties},
+    {Py_tp_methods, (void*)PyIOSocket_methods},
     {0, nullptr},
 };
 
